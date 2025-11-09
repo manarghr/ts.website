1e8711afaf534fbeafc66685995eeb2cd66b4ff2
Belhaddad dania <belhaddad2023@gmail.com>
2025-11-09 16:40:56 +0100

    creation de la fonction analyse_deadlift

---
+++ AI/MediaPipe/exercises/deadlift.py
@@ -0,0 +1,19 @@
+from posture_utils import calculate_angle
+
+def analyse_deadlift(landmarks, mp_pose):
+    shoulder = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,
+                landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
+    hip = [landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].x,
+           landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].y]
+    knee = [landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].x,
+            landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].y]
+
+    back_angle = calculate_angle(shoulder, hip, knee)
+    feedback = []
+    
+    if back_angle < 150:
+        feedback.append(" Dos trop penché — redresse un peu ton buste.")
+    else:
+        feedback.append(" Bonne position du dos !")
+    
+    return feedback
