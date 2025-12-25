b4c34ee402688dcba25f28022f495afac853eb4f
Belhaddad dania <belhaddad2023@gmail.com>
2025-12-25 16:59:47 +0100

    ajout posture angle thresholds dans le fichier posture_utils.py

--- AI/MediaPipe/posture_utils.py
+++ AI/MediaPipe/posture_utils.py
@@ -8,3 +8,24 @@ def calculate_angle(a, b, c):
     if angle > 180.0:
         angle = 360 - angle
     return angle
+
+def get_angle_thresholds(user_level, exercise):
+    thresholds = {
+        "beginner": {
+            "squat": (80, 140),
+            "pushup": (50, 160),
+            "biceps_curl": (50, 150),
+            # Ajoute les autres exercices ici
+        },
+        "intermediate": {
+            "squat": (70, 150),
+            "pushup": (40, 170),
+            "biceps_curl": (40, 160),
+        },
+        "advanced": {
+            "squat": (60, 160),
+            "pushup": (30, 180),
+            "biceps_curl": (30, 170),
+        }
+    }
+    return thresholds[user_level].get(exercise, (80, 140))  # Valeurs par défaut
