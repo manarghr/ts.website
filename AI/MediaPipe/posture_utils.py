12fe55bdadcf6573425fda8db7f5dfcc0b007aeb
Belhaddad dania <belhaddad2023@gmail.com>
2025-12-29 18:11:25 +0100

    ajout de RepetitionCounter et EXERCISE_CONFIG pour cree un compteur de repetitions dans main.py

--- AI/MediaPipe/posture_utils.py
+++ AI/MediaPipe/posture_utils.py
@@ -41,9 +41,35 @@ def get_best_point(landmarks, left_id, right_id):
         return [right.x, right.y, right.z]
 
 
+class RepetitionCounter:
+    def __init__(self, min_angle, max_angle):
+        self.min_angle = min_angle
+        self.max_angle = max_angle
+        self.state = "down"
+        self.count = 0
+
+    def update(self, angle):
+        if angle > self.max_angle:
+            self.state = "down"
+
+        if angle < self.min_angle and self.state == "down":
+            self.state = "up"
+            self.count += 1
+
+        return self.count
 
 
-
-
-
-
+EXERCISE_CONFIG = {
+    "biceps_curl": {
+        "min_angle": 40,
+        "max_angle": 160
+    },
+    "squat": {
+        "min_angle": 70,
+        "max_angle": 170
+    },
+    "pushup": {
+        "min_angle": 60,
+        "max_angle": 160
+    }
+}
