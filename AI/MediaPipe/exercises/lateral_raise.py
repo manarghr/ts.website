611d502d8c5c2fab393423f91d2c5444b9bd8551
Belhaddad dania <belhaddad2023@gmail.com>
2025-12-29 17:37:48 +0100

    amelioration de  l'exercice leg_raise

--- AI/MediaPipe/exercises/lateral_raise.py
+++ AI/MediaPipe/exercises/lateral_raise.py
@@ -1,21 +1,19 @@
 from posture_utils import calculate_angle
 
-def analyse_lateral_raise(landmarks, mp_pose):
-    elbow = [landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].x,
-             landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y]
-    shoulder = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,
-                landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
+def analyse_leg_raise(landmarks, mp_pose):
     hip = [landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].x,
            landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].y]
+    knee = [landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].x,
+            landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].y]
+    ankle = [landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].x,
+             landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].y]
 
-    angle = calculate_angle(elbow, shoulder, hip)
+    angle = calculate_angle(hip, knee, ankle)
     feedback = []
 
-    if angle < 30:
-        feedback.append("Bras trop bas — lève les bras")
-    elif angle > 90:
-        feedback.append("Bras trop haut — descends légèrement")
+    if angle < 150:
+        feedback.append("Garde les jambes tendues")
     else:
-        feedback.append("Élévation latérale correcte")
+        feedback.append("Leg raise correct")
 
     return feedback
