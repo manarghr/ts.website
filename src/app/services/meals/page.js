29f822aded7e120ce2f904990fe315687cfbbbb1
Ferielysf <ysfferiel@gmail.com>
2026-01-05 18:14:11 +0100

    change pics meal

--- src/app/services/meals/page.js
+++ src/app/services/meals/page.js
@@ -351,7 +351,7 @@ useEffect(() => {
         <div 
           className="absolute inset-0 opacity-20"
           style={{
-            backgroundImage: "url('https://images.unsplash.com/photo-1490645935967-10de6ba17061?w=1200')",
+            backgroundImage: "url('https://plus.unsplash.com/premium_photo-1700760417057-bea54a42503c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
             backgroundSize: "cover",
             backgroundPosition: "center",
             filter: "brightness(1.1) contrast(0.85)"
